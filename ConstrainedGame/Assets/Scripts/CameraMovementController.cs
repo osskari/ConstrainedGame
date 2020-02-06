2f8ffcc41b393384597061d39a7334b10d1a4010
Kristinn Björgvinsson <kristinnb17@ru.is>
2020-02-06 20:33:20 +0000

    Different controlls 'ijkl | 8456' split screen

---
+++ ConstrainedGame/Assets/Scripts/CameraMovementController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovementController : MonoBehaviour
+{
+
+    // The object the camera follows
+    public GameObject followObject;
+    public float followTime;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Follow 20% of the way to the player
+        transform.position += (followObject.transform.position - new Vector3(transform.position.x, transform.position.y, followObject.transform.position.z)) * (Time.deltaTime * followTime);
+    }
+}
---
+++ ConstrainedGame/Assets/Scripts/CarMovement1.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement1 : MonoBehaviour
+{
+    private Rigidbody2D body;
+    public float acceleration, steering;
+    private int score;
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float horiz = -Input.GetAxis("Horizontal2");
+        float vert = Input.GetAxis("Vertical2");
+
+        body.AddForce(transform.up * (vert * acceleration * Time.fixedDeltaTime));
+
+        if (Vector2.Dot(body.velocity, body.GetRelativeVector(Vector2.up)) >= 0.0f)
+        {
+            body.rotation += horiz * steering * (body.velocity.magnitude / 5.0f);
+        }
+        else
+        {
+            body.rotation -= horiz * steering * (body.velocity.magnitude / 5.0f);
+        }
+
+        Vector2 perpendicular = Quaternion.AngleAxis(body.angularVelocity > 0 ? -90 : 90, Vector3.forward) * new Vector2(0.0f, 0.5f);
+        float counterSlide = Vector2.Dot(body.velocity, body.GetRelativeVector(perpendicular.normalized));
+
+        AddScore(perpendicular, counterSlide);
+
+        body.AddForce(body.GetRelativeVector(-perpendicular.normalized * counterSlide * (vert == 0 ? 1f : 0.1f)));
+
+    }
+
+    void AddScore(Vector2 perpendicular, float counterSlide)
+    {
+        var counterNormal = Vector2.Dot(body.velocity.normalized, body.GetRelativeVector(perpendicular.normalized));
+        if (Mathf.Abs(counterNormal) > 0.6f)
+        {
+            score += Mathf.Abs(Mathf.RoundToInt((((body.velocity.magnitude * 0.5f) * (counterNormal * 10f)) * 0.1f)));
+        }
+    }
+}
