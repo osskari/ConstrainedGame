ed29b688c6ae0024ec5d346f3e0ea109fe4a3316
Kristinn Björgvinsson <kristinnb17@ru.is>
2020-02-12 18:35:29 +0000

    Changes yes

---
+++ ConstrainedGame/Assets/Scripts/SkidmarkManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkidmarkManager : MonoBehaviour
+{
+    public List<TrailRenderer> wheels;
+
+    void BeginDraw()
+    {
+        for (int i = 0; i < wheels.Count; i++)
+        {
+            wheels[i].emitting = true;
+        }
+    }
+
+    void EndDraw()
+    {
+        for (int i = 0; i < wheels.Count; i++)
+        {
+            wheels[i].emitting = false;
+        }
+    }
+    
+}
