17dd2d1cce0072ecf087b29d45072f51a81bdfed
unknown <>
2020-02-16 11:43:09 +0000

    brake lights

---
+++ ConstrainedGame/Assets/Scripts/CarLightsController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarLightsController : MonoBehaviour
+{
+    public GameObject leftBrake, rightBrake;
+    public KeyCode key, altKey;
+    private Light lbLight, rbLight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lbLight = leftBrake.GetComponent<Light>();
+        rbLight = rightBrake.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(key) || Input.GetKey(altKey))
+        {
+            lbLight.intensity = 4f;
+            rbLight.intensity = 4f;
+        }
+        else
+        {
+            lbLight.intensity = 10f;
+            rbLight.intensity = 10f;
+        }
+    }
+}
